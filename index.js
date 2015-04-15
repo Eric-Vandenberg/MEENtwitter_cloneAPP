6cf9324232a40a886a484d52754a451c9b39ad7c
Eric Vandenberg <Eric1827v@gmail.com>
2015-04-14 20:13:58 -0700

    Setting tweet query

--- index.js
+++ index.js
@@ -6,22 +6,29 @@ var fixtures = require('./fixtures');
 var app = express();
 
 app.get('api/tweets', function(req, res){
+	if(!req.query.userId) {
+  	 return res.sendStatus(400);
+	};
 	var result = [];
 	fixtures.tweets.filter(function(tweet){
 	 if(tweet.userId == req.query.userId){
 	    result.push(tweet);
 	 }
 	});
+	var sortedResult = result.sort(function (a, b) {
+	  if (a.userId > b.userId) {
+	    return 1;
+	  }
+	  if (a.userId < b.userId) {
+	    return -1;
+	  }
+	  // a must be equal to b
+	  return 0;
+	});
 	if(result.length > 0) { 
 		res.sendStatus(200)
-		res.send({tweets: result}); 
+		res.send({tweets: sortedResult}); 
 	} 
-	if(!req.query.userId) {
-  	res.sendStatus(400);
-	};
-	if(!userId) {
-  	res.sendStatus(400);
-	};
 });
  
 // catch 404 and forward to error handler
