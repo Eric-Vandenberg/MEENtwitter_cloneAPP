0e7ebde164fbffdba72bf133b4b8bc22f0e5e877
Eric Vandenberg <Eric1827v@gmail.com>
2015-04-22 16:13:11 -0700

    Installed Passport

--- auth.js
+++ auth.js
@@ -10,17 +10,10 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(id, done) {
 	var userAlready = _.find(fixtures.users, 'id', id);
-	var iD = userAlready.id;
-	console.log("this is the id: ", iD);
+
 	if (!userAlready) {
 		done(null, false)
 	}
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
 });
 
 module.exports = passport;
